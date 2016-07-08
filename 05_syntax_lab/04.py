4a545525d0b7f6a33979f853e69707a4bfe90c32
ronyblum <ronyblum@gmail.com>
2016-07-08 17:52:13 +0300

    Solution for: sol-python-verint.syntax-lab/04.py

--- 05_syntax_lab/04.py
+++ 05_syntax_lab/04.py
@@ -1,8 +1,19 @@
 """
-Write a program that reads lines from the user
-until an empty line is inserted.
-After the user typed in an empty line,
-print all previously inserted lines in reverse
-order (from last to first)
+Exercise 03
+
+Write a Python script reading user input until user enters blank line. When blank line is entered, print all the inputs from the end to the beginning
 """
 
+#from random import randint
+
+text = []
+sent = None
+
+while True:
+	sent = raw_input('Enter a Sentence (When done, press enter): ')
+	text.append(sent)
+	if len(sent) <1: break
+text.reverse();
+print 'Here is the text inserted:'
+for sentence in text:
+	print sentence
